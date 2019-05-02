2927f625db504656f0ade85553f887715e509d20
Carlos Pelezinho <inm.carlosna@gmail.com>
2019-05-02 16:35:24 -0300

    Dockerfile

--- src/main/java/br/com/vagasapi/VagasAPISpringBoot.java
+++ src/main/java/br/com/vagasapi/VagasAPISpringBoot.java
@@ -1,11 +1,13 @@
 package br.com.vagasapi;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableAdminServer
 public class VagasAPISpringBoot {
 
     public static void main(String[] args) {
